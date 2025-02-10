e88b5bde886f5b0db131af7913a589ea5aee8032
George Li <glli@ucsd.edu>
2025-02-09 23:49:05 -0600

    NO-ISSUE update pnpm version

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,6 +1,6 @@
 
 export default new Map([
-["src/data/authors/glli.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fauthors%2Fglli.mdx&astroContentModuleFlag=true")],
 ["src/data/authors/shadcn.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fauthors%2Fshadcn.mdx&astroContentModuleFlag=true")],
+["src/data/authors/glli.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fauthors%2Fglli.mdx&astroContentModuleFlag=true")],
 ["src/data/blogs/example.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fblogs%2Fexample.mdx&astroContentModuleFlag=true")]]);
 		
